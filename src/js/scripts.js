afc2853bdeab93822b3e735cc9e5e0743b4003dc
Mainak Roy <mainak.roy93@gmail.com>
2022-09-27 20:26:16 -0400

    Digital Earth With Atmos

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,9 +2,11 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js' 
 import vertexShader from '../shaders/vertex/globeVertex.glsl'
 import fragmentShader from '../shaders/fragment/globeFragment.glsl'
-// console.log(vertexShader)
+import atmoshereVertexShader from '../shaders/vertex/atmosphereVertex.glsl'
+import atmoshereFragmentShader from '../shaders/fragment/atmosphereFragment.glsl' 
+import camera_properties from '../utils/camera_properties.json'
 
-
+console.log(atmoshereFragmentShader);
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -17,9 +19,37 @@ const renderer = new THREE.WebGL1Renderer({
     "antialias":true
 });
 
+
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
+
+const digital_material = new THREE.MeshPhongMaterial({
+    map: new THREE.TextureLoader().load(require('../img/digitalearth_4.jpg')),
+    // bumpMap: new THREE.TextureLoader().load(require('../img/8081_earthbump10k.jpg')),
+    // specularMap: new THREE.TextureLoader().load(require('../img/8081_earthspec10k.jpg')),
+    // bumpScale:1,
+})
+
+const digital_mesh = new THREE.Mesh(new THREE.SphereGeometry(5,250,250), digital_material)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+const pointLight = new THREE.PointLight(0xffffff,1);
+pointLight.position.set(5,0,9)
+
+const atmosphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5,250,250),
+    // new THREE.MeshBasicMaterial({
+    //     map : new THREE.TextureLoader().load(require('../img/8081_earthmap10k.jpg'))
+    // })
+    new THREE.ShaderMaterial({
+        vertexShader : atmoshereVertexShader,
+        fragmentShader :  atmoshereFragmentShader,
+        blending: THREE.AdditiveBlending,
+        side: THREE.BackSide
+    })
+)
+atmosphere.scale.set(1.4,1.4,1.4)
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5,250,250),
@@ -31,19 +61,29 @@ const sphere = new THREE.Mesh(
         fragmentShader,
         uniforms :{
             globeTexture : {
-                value : new THREE.TextureLoader().load(require('../img/8081_earthmap10k.jpg'))
+                value : new THREE.TextureLoader().load(require('../img/digitalearth_2.jpeg'))
             }
         }
     })
 )
 
-scene.add(sphere)
+scene.add(digital_mesh)
+scene.add(atmosphere)
+scene.add(ambientLight)
+scene.add(pointLight)
 
-camera.position.set(0,0,10)
+const orbit =  new OrbitControls(camera, renderer.domElement)
+orbit.minDistance = camera_properties.minDistance
+orbit.maxDistance = camera_properties.maxDistance
+orbit.zoomSpeed = camera_properties.zoomSpeed
+orbit.panSpeed = camera_properties.panSpeed
+camera.position.set(0,0,11)
 
 function animate() {
 
 	requestAnimationFrame( animate );
+    digital_mesh.rotation.y -= 0.0015
+    orbit.update()
 	renderer.render( scene, camera );
 
 }
